952a18ea8d26c7c10c5795586abadeb447a6f1e6
ersen <70ozturkersen@gmail.com>
2022-07-12 01:47:27 +0300

    mainTopUp bacially created

--- src/components/mainTopUp/MainTopUp.jsx
+++ src/components/mainTopUp/MainTopUp.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import styles from "./MainTopUp.module.css";
+import { BsSearch,BsBell, BsChevronDown } from "react-icons/bs";
 
 const MainTopUp = () => {
   return (
-    <div>MainTopUp</div>
-  )
-}
+    <div className={styles.mainTopUp}>
+      <div className={styles.text}>Arge Projesi’ne Hoşgeldin Örnek İnsan</div>
 
-export default MainTopUp
+      <div className={styles.search}>
+        <div className={styles.icon}>
+          <BsSearch />{" "}
+        </div>
+        <input
+          type="search"
+          name="search"
+          id="search"
+          className={styles.inputSearch}
+          placeholder="Arama yapın..."
+        />
+      </div>
+
+      <div className={styles.bell}>
+        <BsBell />
+        <div className={styles.redPoint}>
+        </div>
+      </div>
+
+      <div className={styles.guest}>
+        <img src="https://www.iconninja.com/files/553/986/399/face-avatar-icon.png" alt="" />
+        <p>Örnek İnsan</p>
+        <BsChevronDown className={styles.downArrow}/>
+
+      </div>
+    </div>
+  );
+};
+
+export default MainTopUp;
