f06174c58a466670cd150c7aab0c2d5dc5b58bfd
ersen <70ozturkersen@gmail.com>
2022-07-12 13:50:32 +0300

    icons packages added

--- src/components/mainTopDown/MainTopDown.jsx
+++ src/components/mainTopDown/MainTopDown.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import styles from "./MainTopDown.module.css";
+import { BsFillFileTextFill } from "react-icons/bs";
+import { RiHistoryLine } from "react-icons/ri";
 
 const MainTopDown = () => {
   return (
-    <div>MainTopDown</div>
-  )
-}
+    <div className={styles.mainTopDown}>
+      <div className={styles.metin}>
+        <div className={styles.metinIcon}>
+          <BsFillFileTextFill />
+        </div>
+        <div className={styles.metinText}>AI Metin Yazarı </div>
+      </div>
+      <div className={styles.gecmis}>
+      <div className={styles.gecmisIcon}>
+          <RiHistoryLine />
+        </div>
+        <div className={styles.gecmisText}>Geçmiş</div>
+      </div>
+    </div>
+  );
+};
 
-export default MainTopDown
+export default MainTopDown;
