7487a9d5773da35f3df0df27f57a174f90a954c1
ersen <70ozturkersen@gmail.com>
2022-07-12 20:00:45 +0300

    correct size scaled

--- src/components/mainTopUp/MainTopUp.jsx
+++ src/components/mainTopUp/MainTopUp.jsx
@@ -1,35 +1,39 @@
 import styles from "./MainTopUp.module.css";
-import { BsSearch,BsBell, BsChevronDown } from "react-icons/bs";
+import { BsSearch, BsBell, BsChevronDown } from "react-icons/bs";
 
 const MainTopUp = () => {
   return (
     <div className={styles.mainTopUp}>
-      <div className={styles.text}>Arge Projesi’ne Hoşgeldin Örnek İnsan</div>
-
-      <div className={styles.search}>
-        <div className={styles.icon}>
-          <BsSearch />{" "}
-        </div>
-        <input
-          type="search"
-          name="search"
-          id="search"
-          className={styles.inputSearch}
-          placeholder="Arama yapın..."
-        />
+      <div className={styles.mainTopUpLeft}>
+        <div className={styles.text}>Arge Projesi’ne Hoşgeldin Örnek İnsan</div>
       </div>
-
-      <div className={styles.bell}>
-        <BsBell />
-        <div className={styles.redPoint}>
+      <div className={styles.mainTopUpRight}>
+        <div className={styles.search}>
+          <div className={styles.icon}>
+            <BsSearch />{" "}
+          </div>
+          <input
+            type="search"
+            name="search"
+            id="search"
+            className={styles.inputSearch}
+            placeholder="Arama yapın..."
+          />
         </div>
-      </div>
 
-      <div className={styles.guest}>
-        <img src="https://www.iconninja.com/files/553/986/399/face-avatar-icon.png" alt="" />
-        <p>Örnek İnsan</p>
-        <BsChevronDown className={styles.downArrow}/>
+        <div className={styles.bell}>
+          <BsBell />
+          <div className={styles.redPoint}></div>
+        </div>
 
+        <div className={styles.guest}>
+          <img
+            src="https://www.iconninja.com/files/553/986/399/face-avatar-icon.png"
+            alt=""
+          />
+          <p>Örnek İnsan</p>
+          <BsChevronDown className={styles.downArrow} />
+        </div>
       </div>
     </div>
   );
