832ccb32296ab72029ec325cd562343077beb852
ersen <70ozturkersen@gmail.com>
2022-07-18 02:23:28 +0300

    SideBar's data added

--- src/helper/DropDownData.jsx
+++ src/helper/DropDownData.jsx
@@ -1,4 +1,14 @@
 import Shopping from "../assets/Shopping";
+import {
+  BsPlusLg,
+  BsFillFileTextFill,
+  BsFileImageFill,
+  BsFillHeartFill,
+} from "react-icons/bs";
+import { RiCompassDiscoverLine, RiFolder3Fill } from "react-icons/ri";
+import { BiTrendingUp } from "react-icons/bi";
+import { MdColorLens } from "react-icons/md";
+import { IoFolderSharp, IoSettings, IoExitOutline } from "react-icons/io5";
 
 export const konuBasligi = [
   {
@@ -68,4 +78,108 @@ export const dilTonu = [
   "Neşeli",
   "Tutkulu",
   "Düşünceli",
+];
+
+export const menuItemData = [
+  {
+    icon: <BsPlusLg />,
+    text: "Yeni Proje",
+    class1: "icon",
+    class1C: 'clickedIcon',
+    class2: "mood2",
+    class2C: "clickedMood",
+    classText: "text",
+    classMenuItem: "menuItem",
+  },
+  {
+    icon: <RiCompassDiscoverLine />,
+    text: "Keşfet",
+    class1: "icon",
+    class2: "mood2",
+    classMenuItem: "menuItem",
+    classText: "text ",
+    class1C: 'clickedIcon',
+    class2C: "clickedMood",
+  },
+  {
+    icon: <BsFillFileTextFill />,
+    text: "AI Metin Yazarı",
+    class1: "icon2 activeIcon",
+    classText: "text activeText",
+    classMenuItem: "menuItem active",
+    class1C: 'icon2 activeIcon',
+
+  },
+  {
+    icon: <BsFileImageFill />,
+    text: "AI Görsel Oluşturucu",
+    classText: "text",
+    class1: "icon2",
+    class1C: "icon2",
+    classMenuItem: "menuItem",
+  },
+
+  {
+    icon: <BiTrendingUp />,
+    text: "Trendler",
+    class1: "icon",
+    class2: "mood2",
+    class1C: 'clickedIcon',
+    class2C: "clickedMood",
+    classText: "text",
+    classMenuItem: "menuItem",
+  },
+  {
+    icon: <MdColorLens />,
+    text: "Renk Analizi",
+    class1: "icon",
+    class2: "mood2",
+    class1C: 'clickedIcon',
+    class2C: "clickedMood",
+    classText: "text",
+    classMenuItem: "menuItem",
+  },
+  {
+    icon: <RiFolder3Fill />,
+    text: "Projelerim",
+    classText: "text",
+    class1: "icon2",
+    class1C: "icon2",
+    classMenuItem: "menuItem",
+  },
+  {
+    icon: <BsFillHeartFill />,
+    text: "Favorilerim",
+    class1: "icon",
+    classText: "text",
+    class2: "mood2",
+    class1C: 'clickedIcon',
+    class2C: "clickedMood",
+    classMenuItem: "menuItem",
+  },
+  {
+    icon: <IoFolderSharp />,
+    text: "Paketler",
+    class1: "icon2",
+    class1C: "icon2",
+    classText: "text",
+    classMenuItem: "menuItem",
+  },
+
+  {
+    icon: <IoSettings />,
+    text: "Ayarlar",
+    class1: "icon2",
+    class1C: "icon2",
+    classText: "text",
+    classMenuItem: "menuItem",
+  },
+  {
+    icon: <IoExitOutline />,
+    text: "Çıkış Yap",
+    classText: "text",
+    class1: "icon2",
+    class1C: "icon2",
+    classMenuItem: "menuItem",
+  },
 ];
